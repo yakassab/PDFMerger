6c98a5d85725aa1133f891149fc266aac56da6d1
Youssef Kassab <21p0010@eng.asu.edu.eg>
2024-06-10 22:01:58 +0300

    first push

--- main.py
+++ main.py
@@ -3,35 +3,32 @@ from PyPDF2 import PdfMerger
 
 # Get the list of all PDF files in the current directory
 pdf_files = [f for f in os.listdir() if f.endswith('.pdf')]
-# put all names of pdfs in a string
 
-output_file_name = "_".join([os.path.splitext(f)[0] for f in pdf_files]) + " MERGED.pdf"
+# Generate the output file name based on the input file names
+output_file_name = "_".join([os.path.splitext(f)[0] for f in pdf_files]) + "_MERGED.pdf"
 
-# Print the list of PDF files
-print(pdf_files)
+# Ensure the file name is not too long
+if len(output_file_name) > 255:
+    output_file_name = output_file_name[:252] + "..."
 
+# Print the list of PDF files and the generated output file name
+print("PDF files:", pdf_files)
+print("Output file name:", output_file_name)
 
 # Create a PdfMerger object
 merger = PdfMerger()
 
-
 # Append all the PDF files to the merger object
 for pdf in pdf_files:
-    merger.append(pdf)
-
-# Write the merged PDF to a file give it the name of all the files
-# merger.write(pdf_files + '.pdf')
-# # Close the PdfFileWriter object
-# merger.close()
+    merger.append(os.path.join(os.getcwd(), pdf))
 
 # Write the merged PDF to a file
 merger.write(output_file_name)
 merger.close()
 
-# delete the original files
+# Delete the original files
 for pdf in pdf_files:
     os.remove(pdf)
 
-
-# Path: main.py
-
+print(f"Merged PDF saved as {output_file_name}")
+print("Original PDF files deleted.")
